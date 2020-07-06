c473ad24e74b6d88cd2fae080a779386247038ee
Dimas <dimas.95@hotmail.com>
2020-07-06 18:14:47 +0200

    Fix syntax errors

--- backend/artifacts/templates/template_santos_endurance.sql
+++ backend/artifacts/templates/template_santos_endurance.sql
@@ -5,20 +5,20 @@ CREATE TABLE `{table_prefix}_teams` (
   `reference_time_offset` INT DEFAULT 0 COMMENT 'Respect track reference',
   `insert_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `update_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  UNIQUE KEY `team_name` (`name`),
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  UNIQUE KEY `team_name` (`name`)
+) ENGINE=InnoDB;
 
 CREATE TABLE `{table_prefix}_drivers` (
   `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `team_id` INT UNSIGNED NULL,
-  `name` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(191) NOT NULL,
   `time_driving` INT UNSIGNED DEFAULT 0,
   `reference_time_offset` INT DEFAULT 0 COMMENT 'Respect team reference',
   `insert_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `update_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   UNIQUE KEY `team_driver` (`name`, `team_id`),
   CONSTRAINT `{table_prefix}_drivers__team_id` FOREIGN KEY (`team_id`) REFERENCES `{table_prefix}_teams` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB;
 
 CREATE TABLE `{table_prefix}_timing_historic` (
   `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -36,9 +36,9 @@ CREATE TABLE `{table_prefix}_timing_historic` (
   `is_stop` TINYINT(1) NOT NULL DEFAULT 0,
   `insert_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `update_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  CONSTRAINT `{table_prefix}_timing__team_id` FOREIGN KEY (`team_id`) REFERENCES `{table_prefix}_teams` (`id`)
+  CONSTRAINT `{table_prefix}_timing__team_id` FOREIGN KEY (`team_id`) REFERENCES `{table_prefix}_teams` (`id`),
   CONSTRAINT `{table_prefix}_timing__driver_id` FOREIGN KEY (`driver_id`) REFERENCES `{table_prefix}_drivers` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB;
 
 CREATE TABLE `{table_prefix}_karts_in` (
   `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -48,7 +48,7 @@ CREATE TABLE `{table_prefix}_karts_in` (
   `insert_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `update_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   CONSTRAINT `{table_prefix}_karts_in__team_id` FOREIGN KEY (`team_id`) REFERENCES `{table_prefix}_teams` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB;
 
 CREATE TABLE `{table_prefix}_karts_out` (
   `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -58,30 +58,30 @@ CREATE TABLE `{table_prefix}_karts_out` (
   `insert_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `update_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   CONSTRAINT `{table_prefix}_karts_out__team_id` FOREIGN KEY (`team_id`) REFERENCES `{table_prefix}_teams` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB;
 
 CREATE TABLE `{table_prefix}_karts_probs` (
   `step` INT UNSIGNED NOT NULL,
   `kart_status` ENUM('unknown', 'good', 'medium', 'bad') NOT NULL DEFAULT 'unknown',
-  `probability` DECIMAL(3, 30) NOT NULL,
+  `probability` DECIMAL(30, 30) NOT NULL,
   `insert_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `update_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`step`, `kart_status`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB;
 
 CREATE TABLE `{table_prefix}_event_config` (
   `name` VARCHAR(191) NOT NULL PRIMARY KEY,
   `value` VARCHAR(255) NULL,
   `insert_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `update_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB;
 
 CREATE TABLE `{table_prefix}_event_stats` (
   `name` VARCHAR(191) NOT NULL PRIMARY KEY,
   `value` VARCHAR(255) NULL,
   `insert_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `update_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB;
 
 CREATE TABLE `{table_prefix}_event_health` (
   `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -91,15 +91,13 @@ CREATE TABLE `{table_prefix}_event_health` (
   `message` VARCHAR(1000) NULL,
   `insert_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `update_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  UNIQUE KEY `category_name` (`category`, `name`),
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  UNIQUE KEY `category_name` (`category`, `name`)
+) ENGINE=InnoDB;
 
 INSERT INTO `{table_prefix}_event_config` (`name`, `value`) VALUES
-  ('track_name', 'santos'),
-  ('race_type', 'endurance'),
   ('race_length', NULL),
   ('race_length_unit', NULL),
-  ('reference_top_teams', NULL);
+  ('reference_time_top_teams', NULL);
 
 INSERT INTO `{table_prefix}_event_stats` (`name`, `value`) VALUES
   ('reference_time', 0),
@@ -108,7 +106,7 @@ INSERT INTO `{table_prefix}_event_stats` (`name`, `value`) VALUES
   ('stage', NULL),
   ('remaining_event', NULL);
 
-INSERT INTO `{table_prefix}_event_health` (`name`, `status`) VALUES
+INSERT INTO `{table_prefix}_event_health` (`category`, `name`, `status`) VALUES
   ('database', 'connection', 'offline'),
   ('crawler', 'internet', 'offline'),
   ('crawler', 'live_timing', 'offline'),
