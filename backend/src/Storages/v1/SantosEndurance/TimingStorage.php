c5efb02f9c7ed56e599c4698318f5f12294a695c
Dimas Munoz <dimas.95@hotmail.com>
2020-07-14 04:22:16 +0200

    Fix query

--- backend/src/Storages/v1/SantosEndurance/TimingStorage.php
+++ backend/src/Storages/v1/SantosEndurance/TimingStorage.php
@@ -15,28 +15,31 @@ class TimingStorage extends AbstractSantosEnduranceStorage
         $connection = $this->getConnection();
         $tablePrefix = $this->getTablesPrefix();
         $stmt = "
-            SELECT
-                se_t.name team_name,
-                se_t.number team_number,
-                se_t.reference_time_offset team_reference_time_offset,
-                se_d.name driver_name,
-                se_d.reference_time_offset driver_reference_time_offset,
-                se_d.time_driving driver_time_driving,
-                se_th.position,
-                se_th.time,
-                MAX(se_th.lap) lap,
-                se_th.gap,
-                se_th.stage,
-                se_th.kart_status,
-                se_th.kart_status_guess,
-                se_th.forced_kart_status,
-                se_th.number_stops,
-                se_th.is_stop
-            FROM `" . $tablePrefix . Tables::SE_TIMING_HISTORIC . "` se_th
-            LEFT JOIN `" . $tablePrefix . Tables::SE_TEAMS . "` se_t ON se_t.id = se_th.team_id
-            LEFT JOIN `" . $tablePrefix . Tables::SE_DRIVERS . "` se_d ON se_th.driver_id = se_d.id
-            GROUP BY se_t.name
-            ORDER BY se_th.lap DESC";
+            SELECT *
+            FROM (
+                SELECT
+                    se_t.name team_name,
+                    se_t.number team_number,
+                    se_t.reference_time_offset team_reference_time_offset,
+                    se_d.name driver_name,
+                    se_d.reference_time_offset driver_reference_time_offset,
+                    se_d.time_driving driver_time_driving,
+                    se_th.position,
+                    se_th.time,
+                    se_th.lap,
+                    se_th.gap,
+                    se_th.stage,
+                    se_th.kart_status,
+                    se_th.kart_status_guess,
+                    se_th.forced_kart_status,
+                    se_th.number_stops,
+                    se_th.is_stop
+                FROM `" . $tablePrefix . Tables::SE_TIMING_HISTORIC . "` se_th
+                LEFT JOIN `" . $tablePrefix . Tables::SE_TEAMS . "` se_t ON se_t.id = se_th.team_id
+                LEFT JOIN `" . $tablePrefix . Tables::SE_DRIVERS . "` se_d ON se_th.driver_id = se_d.id
+                ORDER BY se_th.position ASC, se_th.lap DESC
+            ) se_t
+            GROUP BY se_t.team_name";
         $results = $connection->executeQuery($stmt)->fetchAll();
         $results = empty($results) ? [] : $results;
         $results = $this->castTimingRows($results);
@@ -73,7 +76,7 @@ class TimingStorage extends AbstractSantosEnduranceStorage
             JOIN `" . $tablePrefix . Tables::SE_TEAMS . "` se_t ON se_t.id = se_th.team_id
             LEFT JOIN `" . $tablePrefix . Tables::SE_DRIVERS . "` se_d ON se_th.driver_id = se_d.id
             WHERE se_t.name = :name
-            ORDER BY se_th.lap DESC";
+            ORDER BY se_th.lap ASC";
         $params = [':name' => $name];
         $results = $connection->executeQuery($stmt, $params)->fetchAll();
         $results = empty($results) ? [] : $results;
