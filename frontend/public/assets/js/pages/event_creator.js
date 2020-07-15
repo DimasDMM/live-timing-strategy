1eb1c36a0d75bc9370d24743938069d959545b3b
Dimas <dimas.95@hotmail.com>
2020-07-15 19:20:43 +0200

    Add event-creator page

--- frontend/public/assets/js/pages/event_creator.js
+++ frontend/public/assets/js/pages/event_creator.js
@@ -2,7 +2,7 @@ class EventCreatorPage extends Page {
     constructor(apiUrl) {
         super(apiUrl, true);
         this.initElements();
-        this.initData();
+        this.initEvents();
     }
 
     initElements() {
@@ -11,7 +11,61 @@ class EventCreatorPage extends Page {
         }
     }
 
-    initData() {
-        // Nothing
+    initEvents() {
+        let that = this;
+        $('#btn-create-event').click(function (e) { that.eventCreateEvent(e, that) });
+    }
+
+    eventCreateEvent(e, that) {
+        e.preventDefault();
+        $('#btn-create-event').attr('disabled', 'disabled').html('Enviando...');
+
+        let formData = $('#form-event-creator').serializeArray();
+        let parsedData = {};
+        for (let item of formData) {
+            let val = !isNaN(item['value']) ? parseInt(item['value']) : item['value'];
+            let name = '';
+            if (item['name'].match(/^configuration\_(.+)$/)) {
+                if (!('configuration' in parsedData)) {
+                    parsedData['configuration'] = {};
+                }
+
+                let name = item['name'].match(/^configuration\_(.+)$/)[1];
+                parsedData['configuration'][name] = val;
+            } else {
+                name = item['name'];
+                parsedData[name] = val;
+            }
+        }
+
+        super.sendPostRequest(
+            '/v1/events',
+            parsedData,
+            function (data, textStatus, jqXHR) { that.successCallbackCreateEvent(data, textStatus, jqXHR, that); },
+            function (jqXHR, textStatus, errorThrown) { that.errorCallbackCreateEvent(jqXHR, textStatus, errorThrown, that); }
+        );
+    }
+
+    successCallbackCreateEvent(data, textStatus, jqXHR, that) {
+        $('#btn-create-event').html('Redirigiendo...');
+        window.location.href = '/event-index';
+    }
+
+    errorCallbackCreateEvent(jqXHR, textStatus, errorThrown, that) {
+        if (errorThrown == 'Unauthorized') {
+            window.location.href = '/'; // 401
+        } else {
+            that.displayError('Algo salió mal ¯\\_(ツ)_/¯');
+        }
+
+        $('#btn-create-event').removeAttr('disabled').html('Crear evento');
+    }
+
+    displayError(message) {
+        $('#msg-error').removeClass('hide').html(message);
+    }
+
+    hideError() {
+        $('#msg-error').addClass('hide');
     }
 }
