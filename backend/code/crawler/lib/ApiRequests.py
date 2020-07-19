429a419a4da1bd5ad1ce8138d73c99b91f2dd01b
Dimas <dimas.95@hotmail.com>
2020-07-20 01:26:39 +0200

    Add put

--- backend/code/crawler/lib/ApiRequests.py
+++ backend/code/crawler/lib/ApiRequests.py
@@ -2,35 +2,24 @@ import requests
 import json
 
 class ApiRequests:
-    def __init__(self, base_url: str, debug = False):
-        self.debug = debug
-
-        # Settings
+    def __init__(self, base_url: str, api_token: str):
         self.base_url = base_url
-        self._load_settings()
+        self.api_token = api_token
 
-    def get(self, path):
-        api_token = self.settings['api_token']
-        headers = {'X-Request-Id': api_token, 'Content-Type': 'application/json; charset=utf-8'}
+    def get(self, path: str):
+        headers = {'X-Request-Id': self.api_token, 'Content-Type': 'application/json; charset=utf-8'}
         url = '%s%s' % (self.base_url, path)
         r = requests.get(url, headers=headers)
         return json.loads(r.content)
 
-    def post(self, path, data):
-        api_token = self.settings['api_token']
-        headers = {'X-Request-Id': api_token, 'Content-Type': 'application/json; charset=utf-8'}
+    def post(self, path: str, data):
+        headers = {'X-Request-Id': self.api_token, 'Content-Type': 'application/json; charset=utf-8'}
         url = '%s%s' % (self.base_url, path)
         r = requests.post(url, json=data, headers=headers)
         return json.loads(r.content)
 
-    def put(self, path, data):
-        pass
-
-    def _load_settings(self):
-        if self.file_settings is not None:
-            try:
-                with open(self.file_settings, 'r') as fp:
-                    self.settings = json.load(fp)
-            except Exception as e:
-                print('Error: %s' % str(e))
-                raise e
+    def put(self, path: str, data):
+        headers = {'X-Request-Id': self.api_token, 'Content-Type': 'application/json; charset=utf-8'}
+        url = '%s%s' % (self.base_url, path)
+        r = requests.put(url, json=data, headers=headers)
+        return json.loads(r.content)
