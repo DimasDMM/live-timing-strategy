b3a8d137d3af581075c0fb3aa0fbb4f46673db4b
DimasDMM <dimasmunoz@outlook.com>
2023-03-27 21:02:56 +0200

    Include tracks info and parsers config, and rename some columns

--- data/initial_tables.sql
+++ data/initial_tables.sql
@@ -15,26 +15,38 @@ CREATE TABLE `api_tokens` (
   UNIQUE KEY `token` (`token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `events_index` (
+CREATE TABLE `tracks` (
   `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL,
-  `event_id` VARCHAR(255) NOT NULL,
-  `track_name` VARCHAR(255) NOT NULL,
-  `event_type` VARCHAR(255) NOT NULL,
   `insert_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `update_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  UNIQUE KEY `token` (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `events_index` (
+  `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `track_id` INT UNSIGNED NOT NULL,
+  `code` VARCHAR(255) NOT NULL COMMENT 'Verbose ID to identify an event',
+  `name` VARCHAR(255) NOT NULL,
+  `description` VARCHAR(2000) NOT NULL,
+  `insert_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `update_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  CONSTRAINT `events_index__track_id` FOREIGN KEY (`track_id`) REFERENCES `tracks` (`id`),
+  UNIQUE KEY `code` (`code`),
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `events_teams` (
   `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `event_id` INT UNSIGNED NOT NULL,
+  `code` VARCHAR(255) NULL COMMENT 'Optional unique code given by the live timing',
   `name` VARCHAR(255) NOT NULL,
   `number` INT UNSIGNED NULL,
   `reference_time_offset` INT DEFAULT 0 COMMENT 'Respect track reference',
   `insert_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `update_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `code` (`code`),
   CONSTRAINT `events_teams__event_id` FOREIGN KEY (`event_id`) REFERENCES `events_index` (`id`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -43,7 +55,7 @@ CREATE TABLE `events_drivers` (
   `event_id` INT UNSIGNED NOT NULL,
   `team_id` INT UNSIGNED NULL,
   `name` VARCHAR(255) NOT NULL,
-  `driving_time` INT UNSIGNED DEFAULT 0,
+  `total_driving_time` INT UNSIGNED DEFAULT 0,
   `partial_driving_time` INT UNSIGNED DEFAULT 0,
   `reference_time_offset` INT DEFAULT 0 COMMENT 'Respect track reference',
   `insert_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
@@ -53,7 +65,7 @@ CREATE TABLE `events_drivers` (
   CONSTRAINT `events_drivers__team_id` FOREIGN KEY (`team_id`) REFERENCES `events_teams` (`id`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `events_timing_historic` (
+CREATE TABLE `events_timing_history` (
   `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   `event_id` INT UNSIGNED NOT NULL,
   `team_id` INT UNSIGNED NULL,
@@ -64,11 +76,11 @@ CREATE TABLE `events_timing_historic` (
   `lap` INT UNSIGNED NOT NULL,
   `interval` INT UNSIGNED NOT NULL,
   `interval_unit` ENUM('milli', 'laps') NOT NULL,
-  `stage` ENUM('classification', 'race') NOT NULL,
+  `stage` ENUM('free-practice', 'classification', 'race') NOT NULL,
   `pits` INT UNSIGNED NULL,
   `kart_status` ENUM('unknown', 'good', 'medium', 'bad') NOT NULL DEFAULT 'unknown',
   `fixed_kart_status` ENUM('good', 'medium', 'bad') NULL,
-  `number_stops` INT UNSIGNED NOT NULL DEFAULT 0,
+  `number_pits` INT UNSIGNED NOT NULL DEFAULT 0,
   `insert_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `update_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   CONSTRAINT `events_timing__event_id` FOREIGN KEY (`event_id`) REFERENCES `events_index` (`id`),
@@ -105,10 +117,9 @@ CREATE TABLE `events_settings` (
   `name` ENUM (
     'race_length',
     'race_length_unit',
-    'reference_time_top_teams',
     'karts_in_box',
-    'stop_time',
-    'min_number_stops') NOT NULL,
+    'pit_time',
+    'min_number_pits') NOT NULL,
   `value` VARCHAR(255) NULL,
   `insert_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `update_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -130,6 +141,22 @@ CREATE TABLE `events_metadata` (
   `update_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   CONSTRAINT `events_metadata__event_id` FOREIGN KEY (`event_id`) REFERENCES `events_index` (`id`),
   PRIMARY KEY(`event_id`, `name`)
+) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `events_metadata_history` (
+  `id` INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `event_id` INT UNSIGNED NOT NULL,
+  `name` ENUM(
+    'reference_time',
+    'reference_current_offset',
+    'status',
+    'stage',
+    'remaining_event',
+    'remaining_event_unit') NOT NULL,
+  `value` VARCHAR(255) NULL,
+  `insert_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `update_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  CONSTRAINT `events_metadata__event_id` FOREIGN KEY (`event_id`) REFERENCES `events_index` (`id`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `stats_health` (
@@ -161,9 +188,20 @@ CREATE TABLE `strategy_karts_probs` (
   PRIMARY KEY (`event_id`, `step`)
 ) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+CREATE TABLE `parsers_config` (
+  `event_id` INT UNSIGNED NOT NULL,
+  `key` VARCHAR(255) NOT NULL,
+  `value` VARCHAR(255) NULL,
+  `insert_date` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `update_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
 INSERT INTO `api_tokens` (`token`, `name`, `role`) VALUES
   ('d265aed699f7409ac0ec6fe07ee9cb11', 'Batch', 'batch'),
   ('f9a23e776e199b52f12f60cd1ea0dfc3', 'Dimas', 'admin');
+
+INSERT INTO `tracks` (`name`) VALUES
+  ('Karting Los Santos');
 
 COMMIT;
